aead04649935853803705f292c813cee66e343d7
Albert Sanchez <50465984+AlbertSanchezMejia@users.noreply.github.com>
2022-09-06 04:46:04 -0500

    Modelos, aspecto de juego y menú inicial prototipo

---
+++ MonarcaGame/Assets/Scripts/Scene_Change.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Change : MonoBehaviour
+{
+    [SerializeField] int sceneNumber;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+}
